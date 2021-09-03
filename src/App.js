049e030927b1f092c160e75808c917a55a64232e
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-09-03 16:10:48 -0400

    React Tables Mock Data Example

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>React Mock Data from United States</h1>
-      <h5><a href="https://www.youtube.com/watch?v=hson9BXU9F8&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz&index=3" target="_blank">React Table Tutorial Youtube</a></h5>
+      <h5><a href="https://www.youtube.com/watch?v=hson9BXU9F8&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz&index=3" target="_blank" rel="noreferrer">React Table Tutorial Youtube</a></h5>
      <BasicTable />
     </div>
   );
