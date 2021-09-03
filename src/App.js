4fee77c74b8831b8d19b6a97a317e4be5d1a68f8
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-09-03 15:48:25 -0400

    ReactJS Table Mock Data Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { BasicTable } from './components/BasicTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Mock Data from United States</h1>
+      <h5><a href="https://www.youtube.com/watch?v=hson9BXU9F8&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz&index=3" target="_blank">React Table Tutorial Youtube</a></h5>
+     <BasicTable />
     </div>
   );
 }
---
+++ src/components/columns.js
@@ -0,0 +1,47 @@
+export const COLUMNS =[
+    {
+        Header: 'ID',
+        Footer: 'ID',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        Footer: 'First Name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        Footer: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Email Address',
+        Footer: 'Email Address',
+        accessor: 'email'
+    },
+    {
+        Header: 'City',
+        Footer: 'City',
+        accessor: 'city'
+    },
+    {
+        Header: 'State',
+        Footer: 'State',
+        accessor: 'state'
+    },
+    {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone'
+    },
+    {
+        Header: 'Date of Birth',
+        Footer: 'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Age',
+        Footer: 'Age',
+        accessor: 'age'
+    }
+]
