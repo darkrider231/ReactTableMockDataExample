77b40a4bf830055237a47ab2a4b62ccb45e536e0
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-09-03 16:15:11 -0400

    ReactJS Table Mock Data Example

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BasicTable } from './components/BasicTable';
 function App() {
   return (
     <div className="App">
-      <h1>React Mock Data from United States</h1>
+      <h1>React Mock Data Example from United States</h1>
       <h5><a href="https://www.youtube.com/watch?v=hson9BXU9F8&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz&index=3" target="_blank" rel="noreferrer">React Table Tutorial Youtube</a></h5>
      <BasicTable />
     </div>
